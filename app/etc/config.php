9942ec60f5c7537e0d16331f29cc379b24d175b5
Jader Moura <jader@aswti.com.br>
2021-01-15 17:45:43 -0300

    adicionado o modulo MageSpecialist - developer

--- app/etc/config.php
+++ app/etc/config.php
@@ -130,6 +130,8 @@ return [
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Kp' => 1,
+        'MSP_Common' => 1,
+        'MSP_DevTools' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
